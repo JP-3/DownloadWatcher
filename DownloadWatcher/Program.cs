37e09dbad9f5ea6d060181747aad92d650a0a8b3
JP-3 <johnprout16@gmail.com>
2023-11-25 09:03:16 -0600

    added process check

--- DownloadWatcher/Program.cs
+++ DownloadWatcher/Program.cs
@@ -25,9 +25,10 @@ while (true) { System.Threading.Thread.Sleep(60000); } //infinite loop
 static void OnChanged(object source, FileSystemEventArgs e)
 {
     Email email = new Email();
-    email.SendEmail($"Starting {e.Name}");
+    
     if (e.Name == "RestartTeamViewer.jpg")
     {
+        email.SendEmail($"Starting {e.Name}");
         var processStartInfo = new ProcessStartInfo(@"C:\Plex\RestartTeamViewer.bat");
         processStartInfo.CreateNoWindow = true;
         processStartInfo.UseShellExecute = false;
@@ -35,9 +36,11 @@ static void OnChanged(object source, FileSystemEventArgs e)
         process.StartInfo = processStartInfo;
         process.Start();
         process.WaitForExit();
+        email.SendEmail($"Finished {e.Name}");
     }
     else if (e.Name == "ScanFiles.jpg")
     {
+        email.SendEmail($"Starting {e.Name}");
         var processStartInfo = new ProcessStartInfo(@"C:\Plex\TVEpisodeChecker\TVEpisodeChecker\bin\Debug\net6.0\TVEpisodeChecker.exe");
         processStartInfo.CreateNoWindow = true;
         processStartInfo.UseShellExecute = false;
@@ -46,6 +49,18 @@ static void OnChanged(object source, FileSystemEventArgs e)
         process.Start();
         process.WaitForExit();
         File.Delete(@"C:\Users\sneaker\Downloads\ScanFiles.jpg");
+        email.SendEmail($"Finished {e.Name}");
     }
-    email.SendEmail($"Finished {e.Name}");
+    else if (e.Name == "CheckProcess.jpg")
+    {
+        Process[] processlist = Process.GetProcesses();
+        foreach (Process theprocess in processlist)
+        {
+            if (theprocess.ProcessName == "FileMover")
+            {
+                email.SendEmail($"FileMover UP");
+            }
+            File.Delete(@"C:\Users\sneaker\Downloads\CheckProcess.jpg");
+        }
+    }
 }
